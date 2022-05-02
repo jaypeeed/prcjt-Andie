4fc6a533bbf3ab87e77372b62236f53cb0fd1c48
jaypeeed <johmpete337@gmail.com>
2022-05-02 13:16:10 +0800

    initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: homePage()));
+}
+
+// ignore: camel_case_types
+class homePage extends StatefulWidget {
+  const homePage({Key? key}) : super(key: key);
+
+  @override
+  _homePageState createState() => _homePageState();
+}
+
+// ignore: camel_case_types
+class _homePageState extends State<homePage> {
+  List<String> items = [
+    'Log in as Andie',
+    'Log in as Client',
+    'Log in as Admin'
+  ];
+  String? selectedItem = 'Log in as Andie';
+  List<String> signupItems = ['Sign up as ANDIE', 'Sign up as CLIENT'];
+  String? signupSelectedItem = 'Sign up as ANDIE';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage('assets/background1.png'),
+          fit: BoxFit.cover,
+        )),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                // color: Colors.red,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Container(
+                            // color: Colors.cyanAccent,
+                            child: const Image(
+                              image: AssetImage(
+                                'assets/andie_logo.png',
+                              ),
+                              width: 500,
+                              height: 60,
+                            ),
+                          ),
+                        ),
+                        const Expanded(
+                          flex: 5,
+                          child: SizedBox(
+                            width: 20,
+                            height: 20,
+                          ),
+                        )
+                      ],
+                    ),
+                    Expanded(
+                      flex: 200,
+                      child: Row(
+                        children: const [
+                          Expanded(
+                              flex: 5,
+                              child: SizedBox(
+                                width: 20,
+                                height: 20,
+                              )),
+                          Expanded(
+                            flex: 50,
+                            child: Text(
+                              'WELCOME ANDIE / CLIENT',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 50,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Expanded(
+                flex: 1,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            margin: const EdgeInsets.only(top: 15, right: 20),
+                            child: SizedBox(
+                              width: 200,
+                              height: 150,
+                              child: DropdownButtonFormField<String>(
+                                dropdownColor: Colors.orange[300],
+                                value: selectedItem,
+                                items: items
+                                    .map((item) => DropdownMenuItem(
+                                        value: item,
+                                        child: Text(
+                                          item,
+                                          style: const TextStyle(
+                                              fontSize: 20,
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.bold),
+                                        )))
+                                    .toList(),
+                                onChanged: (item) =>
+                                    setState(() => selectedItem = item),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            margin: const EdgeInsets.only(top: 15, right: 20),
+                            child: SizedBox(
+                              width: 200,
+                              height: 150,
+                              child: DropdownButtonFormField<String>(
+                                dropdownColor: Colors.orange[300],
+                                value: signupSelectedItem,
+                                items: signupItems
+                                    .map((item) => DropdownMenuItem<String>(
+                                        value: item,
+                                        child: Text(
+                                          item,
+                                          style: const TextStyle(
+                                              fontSize: 20,
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.bold),
+                                        )))
+                                    .toList(),
+                                onChanged: (item) =>
+                                    setState(() => signupSelectedItem = item),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    const Text('RIGHT'),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/sign_up_page.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: SignUp()));
+}
+
+class SignUp extends StatefulWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  List<String> items = ['Male', 'Female',];
+  String? selectedItem = 'Male';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/background1.png'),
+              fit: BoxFit.cover,
+            )
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children:  [
+            Expanded(
+              flex: 1,
+              child: SizedBox(
+                width: 450,
+                child: Expanded(
+                  flex:1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children:  const [
+                          SizedBox(width: 5,),
+                          Expanded(
+                            flex:2,
+                            child: Image(
+                              image: AssetImage('assets/andie_logo.png',
+                              ),
+                              width: 500,
+                              height: 60,
+                            ),
+                          ),
+                          Expanded(flex: 5,
+                            child:
+                            SizedBox(width: 20,height: 20,),
+                          )
+                        ],
+                      ),
+                      Expanded( flex: 200,
+                        child: Row(
+                          children: const [
+                            Expanded(flex: 5,
+                                child: SizedBox(width: 20,height: 20,)),
+                            Expanded(
+                              flex: 50,
+                              child: Text(
+                                'WELCOME ANDIE / CLIENT',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 50,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+
+            // ------------------Right Pane Configurations-------------------
+
+            Expanded(
+              flex: 1,
+              child: Container(
+                margin: const EdgeInsets.all(50),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+                child: Expanded(
+                  flex: 1,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 50),
+                        child: Text('Sign-Up as ANDIE!',
+                          style: TextStyle(
+                            fontSize: 50,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                      ),
+                      Container(
+                          margin: const EdgeInsets.only(top: 70, left: 40, right: 40),
+                          child: const TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'First Name',
+                            ),
+                          ),),
+                      Container(
+                        margin: const EdgeInsets.only(top: 30, left: 40, right: 40),
+                        child: const TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Last Name',
+                          ),
+                        ),),
+                      Container(
+                        margin: EdgeInsets.only(top: 30, left: 45),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              flex: 2,
+                              child: DropdownButtonFormField<String>(
+                                dropdownColor: Colors.orange[300],
+                                value: selectedItem,
+                                items: items
+                                    .map((item) => DropdownMenuItem(
+                                    value: item,
+                                    child: Text(item,
+                                      style: const TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold
+                                      )
+                                      ,)
+                                ))
+                                    .toList(),
+                                onChanged: (item)=> setState(()=>selectedItem = item),
+                              ),
+                            ),
+                            Expanded(flex:3,
+                                child: Container(
+                                  margin: const EdgeInsets.only(top: 5, left: 40, right: 40),
+                                  child: const TextField(
+                                    decoration: InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      hintText: 'Age',
+                                    ),
+                                  ),)),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top:30,left: 40, right: 40),
+                        child: const TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Contact Number',
+                          ),
+                        ),),
+                      Container(
+                        margin: EdgeInsets.only(top: 70),
+                          child: Text('or',
+                            style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.w700
+                            ),
+                            )),
+
+                          // BUTTON FOR GOOGLE SIGN IN
+
+                    ],
+                  ),
+                ),
+              ),
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
