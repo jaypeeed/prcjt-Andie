41af925bae39db91ac5f4a357e41aa1bf25ed0ec
jaypeeed <johmpete337@gmail.com>
2022-05-02 22:37:16 +0800

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'changees for this video'),
+      home: const MyHomePage(title: 'Trial change ######'),
     );
   }
 }
